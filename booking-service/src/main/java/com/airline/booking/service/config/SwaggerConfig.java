c87e2d56d03b90f8b2b53d61902dbe2b86253a05
Laxminarayan Chintake <laxminrayan.lexicon@gmail.com>
2026-03-07 22:18:20 +0530

    Added swagger, slides

--- booking-service/src/main/java/com/airline/booking/service/config/SwaggerConfig.java
+++ booking-service/src/main/java/com/airline/booking/service/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.airline.flight.service.config;
+package com.airline.booking.service.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -12,9 +12,9 @@ public class SwaggerConfig {
     public OpenAPI airlineApi() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Flight Service API")
+                        .title("Booking Service APIs")
                         .version("1.0")
-                        .description("API documentation for flight service"));
+                        .description("API documentation for booking service"));
     }
 
 }
